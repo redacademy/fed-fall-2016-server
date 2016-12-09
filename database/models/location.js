073e0f66aff36f758d7c43fb3d8dc49a663d3e15
scottclayton <i@scottc.ca>
2016-12-09 10:36:52 -0800

    update location schema to have amenities et. al.

--- database/models/location.js
+++ database/models/location.js
@@ -2,85 +2,37 @@
 const mongoose = require('mongoose')
 
 const LocationSchema = new mongoose.Schema({
-  placeId: {
-    type: String,
-    unique: true,
-    required: false,    // we might want to change this to true, depending on our Google integration
-  },
+  placeId: { type: String, unique: true, required: false },
   loc: {
-    lng: {
-      type: String,
-    },
-    lat: {
-      type: String,
-    },
-    unique: false,
-    required: true,
+    lng: { type: String, unique: false, required: true },
+    lat: { type: String, unique: false, required: true },
   },
   ratingSummary: {
-    quality: {
-      type: Number,
-      min: 1,
-      max: 3,
-    },
-    clean: {
-      type: Number,
-      min: 1,
-      max: 3,
-    },
-    nursing: {
-      type: Number,
-      min: 1,
-      max: 3,
-    },
-    quiet: {
-      type: Number,
-      min: 1,
-      max: 3,
-    },
+    totalRaters: { type: Number, required: true },
+    quality: { type: Number, required: true },
+    clean: { type: Number, required: true },
+    nursing: { type: Number, required: true },
+    quiet: { type: Number, required: true },
   },
   rating: [{
-    userId: {
-      type: String,
-      unique: true,
-    },
-    quality: {
-      type: Number,
-      min: 1,
-      max: 3,
-    },
-    clean: {
-      type: Number,
-      min: 1,
-      max: 3,
-    },
-    nursing: {
-      type: Number,
-      min: 1,
-      max: 3,
-    },
-    quiet: {
-      type: Number,
-      min: 1,
-      max: 3,
-    },
+    userId: { type: String, unique: true, required: true },
+    quality: { type: Number, min: 1, max: 3, required: true },
+    clean: { type: Number, min: 1, max: 3, required: true },
+    nursing: { type: Number, min: 1, max: 3, required: true },
+    quiet: { type: Number, min: 1, max: 3, required: true },
   }],
-  createdBy: {
-    type: String,
-    required: true,
-    unique: false,
-  },
-  createdAt: {
-    type: Date,
-    unique: false,
-  },
-  modifiedAt: {
-    type: Date,
-    unique: false,
-  },
-  mapPin: {
-    type: String,
-    required: false,
+  createdBy: { type: String, required: true, unique: false },
+  createdAt: { type: Date, unique: false, required: true },
+  mapPin: { type: String, required: true },   // this will need a function to determine what pin to use
+  amenities: {
+    changeTable: { type: Boolean, default: false },
+    keyRequired: { type: Boolean, default: false },
+    nursingRoom: { type: Boolean, default: false },
+    private: { type: Boolean, default: false },
+    strollerAccessible: { type: Boolean, default: false },
+    washroomMen: { type: Boolean, default: false },
+    washroomFamily: { type: Boolean, default: false },
+    washroomWomen: { type: Boolean, default: false },
   },
 })
 
