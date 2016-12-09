410c5acab222b2662b8d94fe4e9e00ae81da6469
scottclayton <i@scottc.ca>
2016-12-09 15:42:48 -0800

    build out location/near/:lng/:lat GET route

--- api-routes/GET/locationNear.js
+++ api-routes/GET/locationNear.js
@@ -1,10 +1,21 @@
 const Location = require('../../database/models/location')
 
 function ApiRoutes(router) {
-  router.get('/api/location/near/:lat/:lng', (req, res) => {
-    Location.find({}, (err, locations) => {
-      res.status(200).json(locations)
-    })
+  router.get('/api/location/near/:lng/:lat', (req, res) => {
+    console.log(req.params)
+    Location.geoNear(
+      {
+        type: 'Point',
+        coordinates: [parseFloat(req.params.lng), parseFloat(req.params.lat)]
+      },
+      {
+        distanceField: 'dist.calculated',
+        includeLocs: 'dist.location',
+        num: 10,
+        spherical: true,
+      }, (err, locations) => {
+        res.status(200).json(locations)
+      })
   })
 }
 
